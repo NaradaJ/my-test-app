9b77e08b005ac3550044b352db747532e798a188
NaradaJ <140716249+NaradaJ@users.noreply.github.com>
2023-12-14 05:25:17 +0530

    To Check issue with <Link to = >

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router} from 'react-router-dom';
 
 const App = () => {
   return (
     <Router>
-      <div className='App'>
-        <Header />
-        <h1>Test strip</h1>
-      </div>
+        <div className='App'>
+          <Header/>
+          <h1>Test strip</h1>
+        </div>
     </Router>
   );
 };
--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import './Header.css'
-//import SearchIcon from '@material-ui/icons/search' 
-
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import './Header.css';
+// import SearchIcon from '@material-ui/icons/search';
 
 const Header = () => {
   return (
     <nav className='header'>
-        <img className='header_logo' src='https://media.gettyimages.com/id/1331491686/vector/element-design.jpg?s=612x612&w=gi&k=20&c=T251aOWYAaYrnHHSRVRZcrloq3wZRgsJ46DR0Qt_sVs=' alt='TechBiz '/>
-        <div className='header_search'>
-            <input type='text' className='header_search_input'/>
-            {/*<SearchIcon className="header_search_icon"/> import search icon*/} 
+      <img
+        className='header_logo'
+        src='https://media.gettyimages.com/id/1331491686/vector/element-design.jpg?s=612x612&w=gi&k=20&c=T251aOWYAaYrnHHSRVRZcrloq3wZRgsJ46DR0Qt_sVs='
+        alt='TechBiz'
+      />
+      <div className='header_search'>
+        <input type='text' className='header_search_input' />
+      </div>
+      <Link to= "/" className='header_links'>
+        <div className='header_options'>
+          <span className='header_optionLineone'>Hello, Welcome to TechBiz</span>
+          <span className='header_optionLinetwo'>Sign In or Sign Out</span>
         </div>
-        <link to="/" className='header_links'>
-            <div className='header_options'>
-                <span className='header_optionLineone'>Hello,Welcome to TechBiz</span>
-                <span className='header_optionLinetwo'>Sign In or Sign Out</span>
-        </div>
-        </link>
+      </Link>  
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
