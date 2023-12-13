231c109095d7756e256ddbd514f31418c8921e5e
NaradaJ <140716249+NaradaJ@users.noreply.github.com>
2023-12-13 09:29:18 +0530

    nvs 16.13.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-// App.js
-
 import React from 'react';
-import { CardElement } from 'react-stripe-elements';
+import { Elements } from 'react-stripe-elements'; // Import Elements from react-stripe-elements
+import CheckoutForm from './CheckoutForm'; // Import your CheckoutForm component
 
 const App = () => {
   return (
     <div>
       <h1>Your React App</h1>
       <form>
-        <CardElement />
+        {/* Wrap your CheckoutForm with Elements to initialize the Stripe context */}
+        <Elements>
+          <CheckoutForm />
+        </Elements>
         <button type="submit">Pay</button>
       </form>
     </div>
